4b50fd3e6c7c38c21e52647cdd17a9ed4019fe71
John Mico Bangit <44288730+Mico00004@users.noreply.github.com>
2018-12-03 17:02:02 +0800

    Update LegitimateCompanies.sol

--- LegitimateCompanies.sol
+++ LegitimateCompanies.sol
@@ -1,7 +1,8 @@
 pragma solidity 0.4.24;
 
 contract LegitimateCompanies{
-   address constant public owner = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+   //We hard code the owner's address for security.
+   address constant public dti = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
    string[] industries = ["Airlines","Hotel","Hospital"];
    bytes allCompanies;
     
@@ -13,7 +14,7 @@ contract LegitimateCompanies{
    }
    
    modifier onlyOwner() {
-     require(msg.sender == owner, "Not authorized.");
+     require(msg.sender == dti, "Not authorized.");
      _;
    }
 
@@ -22,14 +23,14 @@ contract LegitimateCompanies{
 
    function CreateRegistration(uint _businessNumber,string _companyName, uint _industry) public payable {
        require(msg.value == 1 ether);
-       require(msg.sender != owner);
+       require(msg.sender != dti);
        Register memory newRegister;
        newRegister.businessNumber = _businessNumber;
        newRegister.companyName = _companyName;
        newRegister.industry = industries[_industry];
        registers[id] = newRegister;
        newRegister.status = false;
-       owner.transfer(1 ether);
+       dti.transfer(1 ether);
        id++;
     allCompanies =bytes(string(abi.encodePacked(allCompanies,_companyName)));
    }
